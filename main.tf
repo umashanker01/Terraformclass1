7f17f71a6833b5bcd84de312e92503833717ce67
Umashankar <Umashankers580@gmail.com>
2024-04-18 20:07:25 +0530

    created the dynamo DB

--- main.tf
+++ main.tf
@@ -4,8 +4,27 @@ provider "aws" {
 
 
 terraform {
-  backend "s3"
-  bucket = "shankar"
-  key = "terraform.tfstate"
-  region = "us-east-1"
+  backend "s3"{
+   bucket = "shankar"
+   key = "terraform.tfstate"
+   region = "us-east-1"
+   dynamodb_table = "terraform-lock" 
+    encrypt        = true 
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
+   name = "terraform-state-lock-dynamo"
+   hash_key = "LockID"
+   read_capacity = 20
+   write_capacity = 20
+
+   attribute {
+      name = "LockID"
+      type = "S"
+   }
+
+   tags {
+     Name = "Terraform Lock Table"
+   }
 }
